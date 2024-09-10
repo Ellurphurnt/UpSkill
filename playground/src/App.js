ee5e045478eb28ec366f9fa6ab83b711c39f1b50
Ellurphurnt <taljaardelrisa@gmail.com>
2024-09-10 10:12:56 +0200

    init project

---
+++ playground/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './styling/App.scss';
+
+function App() {
+  return (
+    <div class="main-container">
+      <div class="sub-container">
+        <div class="sky">
+          <div class="stars"></div>
+          <div class="stars2"></div>
+          <div class="stars3"></div>
+          <div class="comet"></div>
+        </div>
+      </div>
+
+    <div className='dispArea'>
+      <div className='centerDisp'>
+        <h1> Hello world! </h1>
+
+      </div>
+    </div>
+
+    </div>
+  );
+}
+
+export default App;
